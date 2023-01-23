68b90ad65ca0839572040ad4798f47f27e58d612
ibrahim <ibrahim-ethem-50@hotmail.com>
2023-01-23 14:50:13 +0300

    version added hilt application class

---
+++ app/src/main/java/com/ibrahimethemsen/daylightapp/DayLightApp.kt
@@ -0,0 +1,8 @@
+package com.ibrahimethemsen.daylightapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DayLightApp : Application() {
+}
--- app/src/main/java/com/ibrahimethemsen/daylightapp/presentation/MainActivity.kt
+++ app/src/main/java/com/ibrahimethemsen/daylightapp/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.ibrahimethemsen.daylightapp
+package com.ibrahimethemsen.daylightapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ibrahimethemsen.daylightapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
